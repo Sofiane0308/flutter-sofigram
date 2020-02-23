1938c9c4479a60a1a19548c92df16277a8781978
Sofiane0308 <48534055+Sofiane0308@users.noreply.github.com>
2020-02-23 01:09:29 +0100

    create post 9:06

--- lib/screens/create_post_screen.dart
+++ lib/screens/create_post_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CreatePostScreen extends StatefulWidget {
@@ -8,18 +11,86 @@ class CreatePostScreen extends StatefulWidget {
 }
 
 class _CreatePostScreenState extends State<CreatePostScreen> {
+  _showSelectImageDialog() {
+    return Platform.isIOS ? _iosBottomSheet() : _androidDialog();
+  }
+
+  _iosBottomSheet() {
+    showCupertinoModalPopup(
+        context: context,
+        builder: (BuildContext context) {
+          return CupertinoActionSheet(
+              title: Text('Add photo'),
+              actions: <Widget>[
+                CupertinoActionSheetAction(
+                    onPressed: () => print('Camera'),
+                    child: Text('Take photo')),
+                CupertinoActionSheetAction(
+                    onPressed: () => print('Gallery'),
+                    child: Text('Choose from gallery')),
+              ],
+              cancelButton: CupertinoActionSheetAction(
+                onPressed: () => Navigator.pop(context),
+                child: Text('Cancel'),
+              ));
+        });
+  }
+
+  _androidDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: Text('Add photo'),
+            children: <Widget>[
+              SimpleDialogOption(
+                child: Text('Take photo'),
+                onPressed: () => print('Camera'),
+              ),
+              SimpleDialogOption(
+                child: Text('Choose from gallery'),
+                onPressed: () => print('Gallery'),
+              ),
+              SimpleDialogOption(
+                child: Text('Cancel', style: TextStyle(color: Colors.redAccent),),
+                onPressed: () => Navigator.pop(context),
+              )
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
           centerTitle: true,
           backgroundColor: Colors.white,
           title: Text(
-            'Sofigram',
-            style: TextStyle(
-                color: Colors.black, fontFamily: 'Billabong', fontSize: 35),
-          )),
-      body: Center(child: Text('Create post'),)
-    );
+            'Create post',
+            style: TextStyle(color: Colors.black),
+          ),
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.add), onPressed: () => print('add post'))
+          ],
+        ),
+        body: Column(children: <Widget>[
+          GestureDetector(
+            onTap: _showSelectImageDialog,
+            child: Container(
+              height: width,
+              width: width,
+              color: Colors.grey[300],
+              child: Icon(
+                Icons.add_a_photo,
+                color: Colors.white70,
+                size: 170,
+              ),
+            ),
+          )
+        ]));
   }
 }
