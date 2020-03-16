28a74e55b26ac6b993b9f2d9cb204ddda3d89f1d
Sofiane0308 <48534055+Sofiane0308@users.noreply.github.com>
2020-03-16 22:52:43 +0100

    check

--- functions/index.js
+++ functions/index.js
@@ -10,9 +10,31 @@ exports.onFollowUser = functions.firestore.document('followers/{userId}/userFoll
     const userFeedRef = admin.firestore().collection('feeds').doc(followerId).collection('userFeed');
     const followedUserPostsSnapshot = await followedUserPostsRef.get();
     followedUserPostsSnapshot.forEach(doc => {
-        if (doc.exists){
-            userFeedRef.doc(doc.id).se(doc.data());
+        if (doc.exists) {
+            userFeedRef.doc(doc.id).set(doc.data());
         }
     });
 
-}); 
+});
+
+exports.onUnfollowUer = functions.firestore.document('/followers/{userId}/userFollowers/{followerId}').onDelete(async (snapshot, context) => {
+    const userId = context.params.userId;
+    const followerId = context.params.followerId;
+    const userFeedRef = admin.firestore().collection('feeds').doc(followerId).collection('userFeed').where('authorId', '= =', userId);
+    const userPostsSnapshot = await userFeedRef.get();
+    userPostsSnapshot.forEach(doc => {
+        if(doc.exists){
+            doc.ref.delete();
+        }
+    });
+});
+
+exports.onUploadPost = functions.firestore.document('posts/{userId}/usersPosts/{postId}').onCreate(async (snapshot, context) => {
+    console.log(snapshot.data());
+    const userId = context.params.userId;
+    const postId = context.params.postId;
+    const userFollowersRef = admin.firestore().collection('followers').doc(userId).collection('userFollowers');
+    const userFollowersSnapshot = await userFollowersRef.get();
+    userFollowersSnapshot.forEach(doc => {
+        admin.firestore().collection('feeds').doc(doc.id).collection('userFeed').doc(postId).set(snapshot.data());  }); 
+});
