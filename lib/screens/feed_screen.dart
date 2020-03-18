4b0a8861d5f22beddc8b7401b19dffd443923fb7
Sofiane0308 <48534055+Sofiane0308@users.noreply.github.com>
2020-03-18 22:59:56 +0100

    user profile posts

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -5,6 +5,7 @@ import 'package:sofigram/models/user_model.dart';
 import 'package:sofigram/screens/profile_screen.dart';
 import 'package:sofigram/services/auth_service.dart';
 import 'package:sofigram/services/database_service.dart';
+import 'package:sofigram/widgets/post_view.dart';
 
 class FeedScreen extends StatefulWidget {
   static final id = 'feed_screen';
@@ -32,88 +33,7 @@ class _FeedScreenState extends State<FeedScreen> {
     });
   }
 
-  _buildPost(Post post, User author) {
-    return Column(children: <Widget>[
-      GestureDetector(
-        onTap: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (_) =>
-                    ProfileScreen(currentUserId: widget.currentUserId, userId:post.authorId,))),
-        child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-            child: Row(children: <Widget>[
-              CircleAvatar(
-                radius: 25.0,
-                backgroundColor: Colors.grey,
-                backgroundImage: author.proflieImageUrl.isEmpty
-                    ? AssetImage('assets/images/user_placeholder.jpg')
-                    : CachedNetworkImageProvider(author.proflieImageUrl),
-              ),
-              SizedBox(width: 8.0),
-              Text(
-                author.name,
-                style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
-              ),
-            ])),
-      ),
-      Container(
-        height: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: CachedNetworkImageProvider(post.imageUrl),
-              fit: BoxFit.cover),
-        ),
-      ),
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 8),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
-            Widget>[
-          Row(
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.favorite_border),
-                iconSize: 30.0,
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(Icons.comment),
-                iconSize: 30.0,
-                onPressed: () {},
-              )
-            ],
-          ),
-          Padding(
-              padding: EdgeInsets.symmetric(horizontal: 12.0),
-              child: Text('0 Likes',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
-          SizedBox(
-            height: 4,
-          ),
-          Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(left: 12.0, right: 6.0),
-                child: Text(
-                  author.name,
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Expanded(
-                  child: Text(
-                post.caption,
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-                overflow: TextOverflow.ellipsis,
-              ))
-            ],
-          ),
-          SizedBox(height: 12)
-        ]),
-      )
-    ]);
-  }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -141,9 +61,14 @@ class _FeedScreenState extends State<FeedScreen> {
                               return SizedBox.shrink();
                             }
                             User author = snapshot.data;
-                            return _buildPost(post, author);
+                            return PostView(currentUserId: widget.currentUserId, post: post, author: author,);
                           });
                     }))
-            : Center(child: CircularProgressIndicator()));
+            : Center(
+                child: FlatButton(
+                    onPressed: () {
+                      AuthService.logOut(context);
+                    },
+                    child: Text('data'))));
   }
 }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sofigram/models/post_model.dart';
 import 'package:sofigram/models/user_data.dart';
 import 'package:sofigram/models/user_model.dart';
 import 'package:sofigram/services/database_service.dart';
 import 'package:sofigram/utilities/constants.dart';
+import 'package:sofigram/widgets/post_view.dart';
 
 import 'edit_profile_screen.dart';
 
@@ -19,31 +21,36 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  bool isFollowing = false;
-  int followerCount = 0;
-  int followingCount = 0;
+  bool _isFollowing = false;
+  int _followerCount = 0;
+  int _followingCount = 0;
+  List<Post> _posts = [];
+  int _displayPosts = 0; // 0 - Grid, 1 - Column
+  User _profileUser;
 
-  _followOrUnfollow(){
-    if (isFollowing){
+  _followOrUnfollow() {
+    if (_isFollowing) {
       _unfollowUser();
-    }else{
+    } else {
       _followUser();
     }
   }
 
-  _followUser(){
-    DatabaseService.followUser(currentUserId: widget.currentUserId, userId: widget.userId);
+  _followUser() {
+    DatabaseService.followUser(
+        currentUserId: widget.currentUserId, userId: widget.userId);
     setState(() {
-      isFollowing = true;
-      followerCount ++; 
+      _isFollowing = true;
+      _followerCount++;
     });
   }
 
-  _unfollowUser(){
-    DatabaseService.unfollowUser(currentUserId: widget.currentUserId, userId: widget.userId);
+  _unfollowUser() {
+    DatabaseService.unfollowUser(
+        currentUserId: widget.currentUserId, userId: widget.userId);
     setState(() {
-      isFollowing = false;
-      followerCount --;
+      _isFollowing = false;
+      _followerCount--;
     });
   }
 
@@ -63,10 +70,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
         : Container(
             width: 200,
             child: FlatButton(
-                color: isFollowing ? Colors.grey[200] : Colors.blue,
-                textColor: isFollowing ? Colors.black : Colors.white,
+                color: _isFollowing ? Colors.grey[200] : Colors.blue,
+                textColor: _isFollowing ? Colors.black : Colors.white,
                 onPressed: _followOrUnfollow,
-                child: Text(isFollowing ? 'Unfollow' : 'Follow',
+                child: Text(_isFollowing ? 'Unfollow' : 'Follow',
                     style: TextStyle(fontSize: 18.0))),
           );
   }
@@ -77,31 +84,196 @@ class _ProfileScreenState extends State<ProfileScreen> {
     _setupIsFollowing();
     _setupFollowers();
     _setupFollowing();
-    
+    _setupPosts();
+    _setupProfileUser();
   }
 
   _setupIsFollowing() async {
     bool isFollowingUser = await DatabaseService.isFollowingUser(
-        currentUserId: widget.currentUserId,
-        userId: widget.userId);
-      setState(() {
-        isFollowing = isFollowingUser;
-      });
+        currentUserId: widget.currentUserId, userId: widget.userId);
+    setState(() {
+      _isFollowing = isFollowingUser;
+    });
   }
 
   _setupFollowers() async {
     int userFollowerCount = await DatabaseService.numFollowers(widget.userId);
     setState(() {
-      followerCount = userFollowerCount;
+      _followerCount = userFollowerCount;
     });
   }
 
   _setupFollowing() async {
-
     int userFollowingCount = await DatabaseService.numFollowing(widget.userId);
     setState(() {
-      followingCount = userFollowingCount;
+      _followingCount = userFollowingCount;
     });
+  }
+
+  _setupPosts() async {
+    List<Post> posts = await DatabaseService.getUserPosts(widget.userId);
+    setState(() {
+      _posts = posts;
+    });
+  }
+
+  _setupProfileUser() async {
+    User profileUser = await DatabaseService.getUserWithId(widget.userId);
+    setState(() {
+      _profileUser = profileUser;
+    });
+  }
+
+  _buildProfileInfo(User user) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+          child: Row(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.grey,
+                backgroundImage: user.proflieImageUrl.isEmpty
+                    ? AssetImage('assets/images/user_placeholder.png')
+                    : CachedNetworkImageProvider(user.proflieImageUrl),
+              ),
+              Expanded(
+                child: Column(children: <Widget>[
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              _posts.length.toString(),
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w600),
+                            ),
+                            Text('posts',
+                                style: TextStyle(color: Colors.black54))
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              _followingCount.toString(),
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w600),
+                            ),
+                            Text('following',
+                                style: TextStyle(color: Colors.black54))
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              _followerCount.toString(),
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w600),
+                            ),
+                            Text('followers',
+                                style: TextStyle(color: Colors.black54))
+                          ],
+                        ),
+                      ]),
+                  _displayButton(user)
+                ]),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  user.name,
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8),
+                Container(
+                    height: 80,
+                    child: Text(
+                      user.bio,
+                      style: TextStyle(fontSize: 15),
+                    )),
+                Divider()
+              ]),
+        )
+      ],
+    );
+  }
+
+  _buildToggleButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(Icons.grid_on),
+          iconSize: 30.0,
+          color: _displayPosts == 0
+              ? Theme.of(context).primaryColor
+              : Colors.grey[300],
+          onPressed: () {
+            setState(() {
+              _displayPosts = 0;
+            });
+          },
+        ),
+        IconButton(
+          icon: Icon(Icons.list),
+          iconSize: 30.0,
+          color: _displayPosts == 1
+              ? Theme.of(context).primaryColor
+              : Colors.grey[300],
+          onPressed: () {
+            setState(() {
+              _displayPosts = 1;
+            });
+          },
+        )
+      ],
+    );
+  }
+
+  _buildTilePost(Post post) {
+    return GridTile(
+      child: Image(
+        image: CachedNetworkImageProvider(post.imageUrl),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  _buildDisplayPosts() {
+    if (_displayPosts == 0) {
+      // grid
+      List<GridTile> tiles = [];
+      _posts.forEach((post) => tiles.add(_buildTilePost(post)));
+      return GridView.count(
+        crossAxisCount: 3,
+        childAspectRatio: 1.0,
+        mainAxisSpacing: 2,
+        crossAxisSpacing: 2,
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        children: tiles,
+      );
+    } else {
+      //column
+      List<PostView> postViews = [];
+      _posts.forEach((post) {
+        postViews.add(PostView(
+            currentUserId: widget.currentUserId,
+            post: post,
+            author: _profileUser));
+      });
+      return Column(
+        children: postViews,
+      );
+    }
   }
 
   @override
@@ -128,85 +300,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
             print(user.email);
             return ListView(
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
-                  child: Row(
-                    children: <Widget>[
-                      CircleAvatar(
-                        radius: 50,
-                        backgroundColor: Colors.grey,
-                        backgroundImage: user.proflieImageUrl.isEmpty
-                            ? AssetImage('assets/images/user_placeholder.png')
-                            : CachedNetworkImageProvider(user.proflieImageUrl),
-                      ),
-                      Expanded(
-                        child: Column(children: <Widget>[
-                          Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Column(
-                                  children: <Widget>[
-                                    Text(
-                                      '12',
-                                      style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                    Text('posts',
-                                        style: TextStyle(color: Colors.black54))
-                                  ],
-                                ),
-                                Column(
-                                  children: <Widget>[
-                                    Text(
-                                      followingCount.toString(),
-                                      style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                    Text('following',
-                                        style: TextStyle(color: Colors.black54))
-                                  ],
-                                ),
-                                Column(
-                                  children: <Widget>[
-                                    Text(
-                                      followerCount.toString(),
-                                      style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                    Text('followers',
-                                        style: TextStyle(color: Colors.black54))
-                                  ],
-                                ),
-                              ]),
-                          _displayButton(user)
-                        ]),
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          user.name,
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 8),
-                        Container(
-                            height: 80,
-                            child: Text(
-                              user.bio,
-                              style: TextStyle(fontSize: 15),
-                            )),
-                        Divider()
-                      ]),
-                )
+                _buildProfileInfo(user),
+                _buildToggleButtons(),
+                Divider(),
+                _buildDisplayPosts()
               ],
             );
           }),
